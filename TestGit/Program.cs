fe8a226214b80e0cb72b23cc45a5aa568e7db151
tsyronok14 <yanochka.ts@bk.ru>
2020-09-06 17:10:30 +0300

    Back to world

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Mindbox!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
